93108caf4feb4f7a9fba6b29e54a163eeb330221
ehendrickson2 <edwinhendrickson2@gmail.com>
2024-12-18 13:56:45 -0600

    Updated models to add more of the data fields and relationships.

--- agon/tournament-bracket/models.py
+++ agon/tournament-bracket/models.py
@@ -1,44 +1,62 @@
+"""Tournament Bracket data models."""
+
 from django.db import models
-from django.db.models import 
 from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    """Class for Custom User model."""
+
 
 class Event(models.Model):
     """Class for top level Events, that contain one or multiple Tournaments."""
 
     name = models.CharField(max_length=50, help_text="Name of Event")
-    description = models.CharField(max_length=250, help_text="Short description of Event")
-    
-    # One to Many relationship to Tournaments class
-    #tournaments = models.ForeignKey()
+    short_description = models.CharField(
+        max_length=250, help_text="Short description of Event"
+    )
+    description = models.TextField()
+    url = models.URLField()
+    venue = models.CharField(max_length=50, help_text="Venue name or Address")
 
     class Meta:
+        """Meta for Event class."""
+
         ordering = [
-            "name",
             "pk",
+            "name",
+            "short_description",
             "description",
+            "url",
+            "venue",
         ]
-    
+
     # Methods
     def __str__(self):
         """String for representing Event object."""
-        return self.name
+        return f"{self.name}"
+
 
 class Tournament(models.Model):
     """Class for Tournaments, that contain one or many brackets."""
 
     name = models.CharField(max_length=50, help_text="Name of Tournament")
+    description = models.TextField()
+    url = models.URLField()
+    host = models.CharField(max_length=50, help_text="Host of this tournament bracket")
+    # Many to Many to game class
+    # game = models.ManyToManyField(Games)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
 
     class Meta:
+        """Meta for Tournament class."""
+
         ordering = [
-            "name",
             "pk",
+            "name",
         ]
-    
+
     def __str__(self):
         """String representation."""
-        return self.name
+        return f"{self.name}"
